e681f967e359c7d5cb3312172d726f227d0e6e54
Ice <moobinkung@windowslive.com>
2016-03-04 00:58:54 +0700

    [#9] updated header file and created device_info

--- lib/header.py
+++ lib/header.py
@@ -1,35 +1,35 @@
-import json
+import json, device
 
-def sendText(sender_id, recv_id):
+def sendText(recv_id):
 	x = {}
 	x['process'] = 21
 	x['process-description'] = 'send text to server'
-	x['sender'] = sender_id
+	x['sender'] = device.get_full_id()
 	x['receiver'] = recv_id
 	x['path'] = []
 	return json.dumps(x)
 
-def forwardData(old_header, forward_id):
+def forwardData(old_header):
 	x = json.loads(old_header)
-	x['path'].append(forward_id)
+	x['path'].append(device.get_full_id())
 	return json.dumps(x)
 
-def sendFile(sender_id, recv_id, file_name, buffer_size):
+def sendFile(recv_id, file_name, buffer_size):
 	x = {}
 	x['process'] = 20
 	x['process-description'] = 'send file to server'
-	x['sender'] = sender_id
+	x['sender'] = device.get_full_id()
 	x['receiver'] = recv_id
 	x['path'] = []
 	x['file_name'] = file_name
 	x['buffer_size'] = buffer_size
 	return json.dumps(x)
 
-def registerDevice(device_type, device_id):
+def registerDevice():
 	x = {}
 	x['process'] = 40
 	x['process-description'] = 'register device'
-	x['device_id'] = device_id
-	x['device_type'] = device_type
+	x['device_id'] = device.get_full_id()
+	x['device_type'] = device.get_type()
 	x['path'] = []
 	return json.dumps(x)
